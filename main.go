bcfbd7cfb22705b3719ae1623f7428e422014cc6
Ankit Arora <ankit.arora@hpe.com>
2023-09-06 18:05:05 +0530

    new commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"encoding/json"
+	"fmt"
+)
+// ###################################### Main Function ######################################
+func main() {
+	
+	var json_data = make(map[string]string)
+	json_data["last"] = "arora"
+	json_data["no1"] = "ankit"
+	json_data["age"] = "28"
+	json_data["zebra"] = "stripes"
+	json_content,err := json.Marshal(json_data)
+	fmt.Println(string(json_content))
+	
+	return
+}
+
